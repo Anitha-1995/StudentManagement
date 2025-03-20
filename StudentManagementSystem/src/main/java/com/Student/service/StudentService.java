013195542a91b7e482b1c3dc97afa963ec742f86
Anitha-1995 <anithaniii.15@gmail.com>
2025-03-20 16:50:58 +0530

    First commit

---
+++ StudentManagementSystem/src/main/java/com/Student/service/StudentService.java
@@ -0,0 +1,61 @@
+package com.Student.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Student.entity.Student;
+import com.Student.repository.StudentRepository;
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentRepository repo;
+	
+	//create
+	public void saveStudent(Student student) {
+		repo.save(student);
+	}
+	//select
+	public List<Student> getAllStudent(){
+		return repo.findAll();
+	}
+	/*
+	public Optional<Student> getById(long id){
+		return Optional.of(repo.findById(id).orElse(null));
+	}
+	*/
+	public Student getById(long id) {
+		Student student=repo.findById(id).orElse(null);
+		return student;
+	}
+	
+	public boolean updateStudent(long id,Student updateStudent) {
+		Optional<Student>existingStudent = Optional.of(repo.findById(id).orElse(null));
+		
+		if(existingStudent.isPresent()) {
+			Student student=existingStudent.get();
+			student.setName(updateStudent.getName());
+			student.setEmail(updateStudent.getEmail());
+			repo.save(student);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean deleteStudent(Long id) {
+		if(repo.existsById(id)) {
+			repo.deleteById(id);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+
